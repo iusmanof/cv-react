581ec21ea32ab975dbb8b64bf38ea1a934910f00
user <user@WS-1C5BEF2AFE.belinvestbank.by>
2022-03-01 15:27:50 +0300

    feat: implement Home, About

--- cv/src/components/About/About.tsx
+++ cv/src/components/About/About.tsx
@@ -9,6 +9,88 @@ interface IProps {}
  **/
 
 const About: FC<IProps> = (props) => {
-    return <div>About</div>;
+    return (
+        <div className="about">
+            <h2 className="about__header">About me ...</h2>
+            <p className="about__content">
+                <p>
+                    {" "}
+                    My name’s Igor Usmanov I’m from Belarus. I grew up and live in Minsk I’m 30
+                    years old. I love playing sports, swimming in the pool and learning chess I
+                    enjoy studying and lately I have been deeply immersed in programming and I study
+                    application development from start to finish.
+                </p>
+                <p>
+                    Specifically, I like creating something out of nothing. There is an idea and you
+                    build it from the ground up. When you start, there is just a blank text editor
+                    like a blank sheet of paper. When you’re done, there’s a working program in the
+                    end. I like research. Programming tends to involve much googling and reading
+                    through documentation.
+                </p>
+            </p>
+            <h2 className="about__header">Skills...</h2>
+            <p className="about__content">
+                <ul>
+                    <li>
+                        I know the basics of the web direction and use
+                        <b>HTML</b>,<b>CSS</b>,<b>SASS(SCSS)</b>
+                        <b>BEM</b>
+                    </li>
+
+                    <li>
+                        I am constantly updating my knowledge in JavaScript:
+                        <b>ES5, ES6+</b>,<b>Patterns & OOP & SOLID</b>,<b>Websockets</b>
+                        <b>...</b>
+                    </li>
+
+                    <li>
+                        {" "}
+                        I have experience in developing using popular frontend frameworks(
+                        <b>React</b>,<b>Readux toolkit</b>,<b>Flux</b>)
+                    </li>
+
+                    <li>
+                        {" "}
+                        I created RESTful API and have experience in developing using popular
+                        backend frameworks (<b>Node</b>
+                        <b>Express</b>,<b>Nest.js</b>, )
+                    </li>
+
+                    <li>
+                        I used DB (<b>PostgreSQL</b>
+                        <b>MongoDB</b>) in my projects with <b>Typeorm</b>
+                    </li>
+
+                    <li>
+                        Initial experience writing automated tests (<b>Jest</b>,<b>Enzyme</b>,
+                        <b>React Testing Library</b>
+                        <b>Mocha</b>)
+                    </li>
+
+                    <li>
+                        I also use tools like:
+                        <b>git</b>,<b>webpack</b>,<b>TypeScript</b>
+                    </li>
+
+                    <li>
+                        I have little experience in creating / editing containers, as well as their
+                        assembly / disassembly (<b>Docker</b>)
+                    </li>
+
+                    <li>
+                        Used DB (<b>PostgreSQL</b>
+                        <b>MongoDB</b>) in my projects with <b>Typeorm</b>
+                    </li>
+
+                    <li>
+                        I check the code with
+                        <b>Eslint</b>,<b>Eslint with Airbnb</b>,<b>Prettier</b>
+                    </li>
+
+                    <li>I have some experience of joint development with the team</li>
+                </ul>
+            </p>
+        </div>
+    );
 };
 export default About;
--- cv/src/components/Home/Home.tsx
+++ cv/src/components/Home/Home.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {FC} from "react";
 import "./Home.scss";
 
-const Home = () => (
+interface IProps {}
+
+/**
+ * @author
+ * @function @Home
+ **/
+
+const Home: FC<IProps> = () => (
     <div className="home">
         <h1 className="home__title">Ihar Usmanau</h1>
         <h2 className="home__subtitle">Frontend Developer/React Developer</h2>
