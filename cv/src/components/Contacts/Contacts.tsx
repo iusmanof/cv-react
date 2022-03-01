10f82c6413108faba8f38f5738c0eb1b5eaf740f
user <user@WS-1C5BEF2AFE.belinvestbank.by>
2022-03-01 16:14:53 +0300

    feat: implement Contacts

--- cv/src/components/Contacts/Contacts.tsx
+++ cv/src/components/Contacts/Contacts.tsx
@@ -9,7 +9,36 @@ interface IProps {}
  **/
 
 const Contacts: FC<IProps> = (props) => {
-    return <div>Contacts</div>;
+    return (
+        <div>
+            {" "}
+            <div className="contacts">
+                <h2 className="contacts__header">Contacts</h2>
+                <p className="contacts__content">
+                    <ul>
+                        gmail:
+                        <li>iusmanof@gmail.com</li>
+                        <li>
+                            <a href="https://vk.com/iusmanof">vk link</a>
+                        </li>
+                        <li>
+                            <a href="#">facebook link</a>
+                        </li>
+                        <li>
+                            <a href="#">linkedin link</a>
+                        </li>
+                        <li>
+                            <a href="#">discord link</a>
+                        </li>
+                        <li>
+                            <a href="#">telegram link</a>
+                        </li>
+                        <li>Phone: +375 44 566 45 61</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Contacts;
