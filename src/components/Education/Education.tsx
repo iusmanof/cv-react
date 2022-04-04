e8041a82a9792dd483b1052cd697ba1519b4142f
user <user@WS-1C5BEF2AFE.belinvestbank.by>
2022-04-04 15:02:42 +0300

    fix: delete unusful links

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,16 +1,6 @@
 import "./Education.scss";
 
-import ccna_certificate from "../../images/certificate/ccna/ccna.jpg";
-import ccna_certificate_pdf from "../../images/certificate/ccna/ccna.pdf";
-import itessential_certificate from "../../images/certificate/it-essential/it-essential.jpg";
-import itessential_certificate_pdf from "../../images/certificate/it-essential/it-essential.pdf";
-import js_certificate from "../../images/certificate/js-fronend2021q1/js-fronend2021q1-1.jpg";
-import js_certificate_pdf from "../../images/certificate/js-fronend2021q1/js-fronend2021q1.pdf";
-import nodejs_certificate from "../../images/certificate/nodejs2021q4/nodejs2021q4-1.jpg";
-import nodejs_certificate_pdf from "../../images/certificate/nodejs2021q4/nodejs2021q4.pdf";
-
 import {useSpring, animated} from "react-spring";
-import ICertificateInfo from "../../intefaces/ICertificateInfo";
 import EducationList from "../EducationList/EducationList";
 import CertificateList from "../CertificateList/CertificateList";
 
